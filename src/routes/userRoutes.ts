c0e8e0ec5845655dcd8f15d9a9b9f3b387680392
Davwed0 <f1lbertd@connect.hku.hk>
2024-10-21 17:16:10 +0800

    add create, update user route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -13,4 +13,31 @@ userRouter.get('/:userId', async (req, res) => {
     }
 });
 
+userRouter.post('/create', async (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        res.status(400).send('Name is required');
+        return;
+    }
+    const user = await db.createUpdateUser(name);
+    res.json(user);
+})
+
+userRouter.post('/update/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const user = await db.getUser(userId);
+    if (user) {
+        const { name } = req.body;
+        if (!name) {
+            res.status(400).send('Name is required');
+            return;
+        }
+        user.name = name;
+        await db.AppDataSource.manager.save(user);
+        res.json(user);
+    } else {
+        res.status(404).send('User not found');
+    }
+});
+
 export default userRouter;
