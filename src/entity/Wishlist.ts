5a6ee70ae5372524c12840d6d100bd11cf092560
Abel <u3583434@connect.hku.hk>
2024-11-11 12:12:16 +0800

    add primary key for wishlist

--- src/entity/Wishlist.ts
+++ src/entity/Wishlist.ts
@@ -2,13 +2,17 @@ import {
     Column,
     Entity,
     JoinColumn,
-    ManyToOne
+    ManyToOne,
+    PrimaryGeneratedColumn
   } from "typeorm";
 import { User } from "./User";
 import { Property } from "./Property";
 
 @Entity()
 export class Wishlist {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+  
   @Column()
   folderName: string;
 
