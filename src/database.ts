68826147484c4a311b16aecc24314f97ad8fe676
Abel <u3583434@connect.hku.hk>
2024-11-11 11:48:16 +0800

    add wishlist to database.ts

--- src/database.ts
+++ src/database.ts
@@ -5,13 +5,14 @@ import { Group } from "./entity/Group";
 import { Review } from "./entity/Review";
 import { DataSource } from "typeorm";
 import { Property } from "./entity/Property";
+import { Wishlist } from "./entity/Wishlist";
 
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./chat.db",
   synchronize: true,
   logging: false,
-  entities: [User, Message, Group, Review, Property],
+  entities: [User, Message, Group, Review, Property, Wishlist],
 });
 
 export async function initDatabase() {
