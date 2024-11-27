16ba4ec51b484162fda53c5e16a909668abbb3cf
Arnav Varshney <varshney.arnav@gmail.com>
2024-11-27 18:00:42 +0800

    add vrImageUrls

--- src/routes/propertyRoutes.ts
+++ src/routes/propertyRoutes.ts
@@ -21,6 +21,7 @@ interface PropertyType {
   buildingDirection: string;
   estate: string;
   imageUrls?: string[];
+  vrImageUrls: { name: string; url: string }[];
   transactionHistory?: { date: string; price: number }[];
   agentId: string;
   contractType: string;
@@ -29,14 +30,16 @@ interface PropertyType {
   isActive: boolean;
 }
 
-
 export async function createProperty(data: PropertyType) {
   const property = AppDataSource.manager.create(Property, data);
   await AppDataSource.manager.save(property);
   return property;
 }
 
-export async function updateProperty(propertyId: string, data: Partial<PropertyType>) {
+export async function updateProperty(
+  propertyId: string,
+  data: Partial<PropertyType>,
+) {
   const property = await AppDataSource.manager.findOne(Property, {
     where: { id: propertyId },
   });
@@ -74,15 +77,27 @@ propertyRouter.get("/:propertyId", async (req, res) => {
 
 propertyRouter.post("/", async (req, res) => {
   const requiredFields = [
-    "name", "address", "area", "district", "subDistrict", "saleableArea",
-    "saleableAreaPricePerSquareFoot", "grossFloorArea",
-    "grossFloorAreaPricePerSquareFoot", "netPrice", "buildingAge",
-    "buildingDirection", "estate", "agentId", "propertyType", "amenities",
-    "contractType"
+    "name",
+    "address",
+    "area",
+    "district",
+    "subDistrict",
+    "saleableArea",
+    "saleableAreaPricePerSquareFoot",
+    "grossFloorArea",
+    "grossFloorAreaPricePerSquareFoot",
+    "netPrice",
+    "buildingAge",
+    "buildingDirection",
+    "estate",
+    "agentId",
+    "propertyType",
+    "amenities",
+    "contractType",
   ];
 
   for (const field of requiredFields) {
-    if (req.body[field] !== undefined) {
+    if (req.body[field] === undefined) {
       res.status(400).send(`${field} is required`);
       return;
     }
@@ -95,83 +110,129 @@ propertyRouter.post("/", async (req, res) => {
 propertyRouter.post("/query", async (req, res) => {
   try {
     const query = req.body;
-    let queryBuilder = AppDataSource.manager.createQueryBuilder(Property, "property").leftJoinAndSelect("property.agent", "agent");
-
+    let queryBuilder = AppDataSource.manager
+      .createQueryBuilder(Property, "property")
+      .leftJoinAndSelect("property.agent", "agent");
 
     if (query.name) {
-      queryBuilder.andWhere("property.name LIKE :name", { name: `%${query.name}%` });
+      queryBuilder.andWhere("property.name LIKE :name", {
+        name: `%${query.name}%`,
+      });
     }
     if (query.address) {
-      queryBuilder.andWhere("property.address LIKE :address", { address: `%${query.address}%` });
+      queryBuilder.andWhere("property.address LIKE :address", {
+        address: `%${query.address}%`,
+      });
     }
     if (query.area) {
       if (query.area !== "any") {
-        queryBuilder.andWhere("property.area LIKE :area", { area: `%${query.area}%` });
+        queryBuilder.andWhere("property.area LIKE :area", {
+          area: `%${query.area}%`,
+        });
       }
     }
     if (query.district) {
       if (query.district !== "any") {
-        queryBuilder.andWhere("property.district LIKE :district", { district: `%${query.district}%` });
+        queryBuilder.andWhere("property.district LIKE :district", {
+          district: `%${query.district}%`,
+        });
       }
     }
     if (query.subDistrict) {
       if (query.subDistrict !== "any") {
-        queryBuilder.andWhere("property.subDistrict LIKE :subDistrict", { subDistrict: `%${query.subDistrict}%` });
+        queryBuilder.andWhere("property.subDistrict LIKE :subDistrict", {
+          subDistrict: `%${query.subDistrict}%`,
+        });
       }
     }
     if (query.estate) {
-      queryBuilder.andWhere("property.estate LIKE :estate", { estate: `%${query.estate}%` });
+      queryBuilder.andWhere("property.estate LIKE :estate", {
+        estate: `%${query.estate}%`,
+      });
     }
     if (query.propertyType) {
       if (query.propertyType !== "any") {
-        queryBuilder.andWhere("property.propertyType LIKE :propertyType", { propertyType: `%${query.propertyType}%` });
+        queryBuilder.andWhere("property.propertyType LIKE :propertyType", {
+          propertyType: `%${query.propertyType}%`,
+        });
       }
     }
     if (query.contractType) {
-      queryBuilder.andWhere("property.contractType LIKE :contractType", { contractType: `%${query.contractType}%` });
+      queryBuilder.andWhere("property.contractType LIKE :contractType", {
+        contractType: `%${query.contractType}%`,
+      });
     }
 
     if (query.isActive) {
-      queryBuilder.andWhere("property.isActive = :isActive", { isActive: query.isActive });
+      queryBuilder.andWhere("property.isActive = :isActive", {
+        isActive: query.isActive,
+      });
     }
 
     if (query.saleableArea) {
       const { min, max } = query.saleableArea;
       if (min !== undefined && max !== undefined) {
-        queryBuilder.andWhere("property.saleableArea BETWEEN :minArea AND :maxArea", { minArea: min, maxArea: max });
+        queryBuilder.andWhere(
+          "property.saleableArea BETWEEN :minArea AND :maxArea",
+          { minArea: min, maxArea: max },
+        );
       } else if (min !== undefined) {
-        queryBuilder.andWhere("property.saleableArea >= :minArea", { minArea: min });
+        queryBuilder.andWhere("property.saleableArea >= :minArea", {
+          minArea: min,
+        });
       } else if (max !== undefined) {
-        queryBuilder.andWhere("property.saleableArea <= :maxArea", { maxArea: max });
+        queryBuilder.andWhere("property.saleableArea <= :maxArea", {
+          maxArea: max,
+        });
       }
     }
 
     if (query.netPrice) {
       const { min, max } = query.netPrice;
       if (min !== undefined && max !== undefined) {
-        queryBuilder.andWhere("property.netPrice BETWEEN :minPrice AND :maxPrice", { minPrice: min, maxPrice: max });
+        queryBuilder.andWhere(
+          "property.netPrice BETWEEN :minPrice AND :maxPrice",
+          { minPrice: min, maxPrice: max },
+        );
       } else if (min !== undefined) {
-        queryBuilder.andWhere("property.netPrice >= :minPrice", { minPrice: min });
+        queryBuilder.andWhere("property.netPrice >= :minPrice", {
+          minPrice: min,
+        });
       } else if (max !== undefined) {
-        queryBuilder.andWhere("property.netPrice <= :maxPrice", { maxPrice: max });
+        queryBuilder.andWhere("property.netPrice <= :maxPrice", {
+          maxPrice: max,
+        });
       }
     }
 
     if (query.buildingAge) {
       const { min, max } = query.buildingAge;
       if (min !== undefined && max !== undefined) {
-        queryBuilder.andWhere("property.buildingAge BETWEEN :minAge AND :maxAge", { minAge: min, maxAge: max });
+        queryBuilder.andWhere(
+          "property.buildingAge BETWEEN :minAge AND :maxAge",
+          { minAge: min, maxAge: max },
+        );
       } else if (min !== undefined) {
-        queryBuilder.andWhere("property.buildingAge >= :minAge", { minAge: min });
+        queryBuilder.andWhere("property.buildingAge >= :minAge", {
+          minAge: min,
+        });
       } else if (max !== undefined) {
-        queryBuilder.andWhere("property.buildingAge <= :maxAge", { maxAge: max });
+        queryBuilder.andWhere("property.buildingAge <= :maxAge", {
+          maxAge: max,
+        });
       }
     }
 
-    if (query.amenities && Array.isArray(query.amenities) && query.amenities.length > 0) {
+    if (
+      query.amenities &&
+      Array.isArray(query.amenities) &&
+      query.amenities.length > 0
+    ) {
       query.amenities.map((amenity: string, index: number) => {
         const param = `amenity${index}`;
-        queryBuilder.andWhere(`property.amenities LIKE :${param}`, { [param]: `%${amenity}%` });
+        queryBuilder.andWhere(`property.amenities LIKE :${param}`, {
+          [param]: `%${amenity}%`,
+        });
       });
     }
 
